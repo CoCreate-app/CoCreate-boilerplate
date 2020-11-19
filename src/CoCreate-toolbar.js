728ed04a1a8a61bede77e3b8e0a02af70f104246
Frank Pagan <frank@cocreate.app>
2020-11-19 18:55:50 +0000

    set tag name if not the same

--- src/CoCreate-toolbar.js
+++ src/CoCreate-toolbar.js
@@ -1,65 +1,64 @@
+function toolbar(selector, event, frame, elementConfig) {
+  let Window, Document, frameElement;
+  if (!frame) {
+    frameElement = frame.body;
+    Document = document;
+    Window = window;
+  } else {
+    frameElement = frame;
+    Window = frame.contentWindow;
+    Document = Window.document || Window.contentDocument;
+  }
 
-function toolbar(selector, event, frame) {
-  try {
-    
-    let Window,Document, frameElement;
-    if(!frame )
-    {
-      frameElement = frame.body;
-      Document = document
-      Window = window;
-    }
-    else
-    {
-      frameElement = frame;
-      Window = frame.contentWindow;
-      Document = Window.document || Window.contentDocument;
-    }
-       
+  let box = document.querySelector(selector);
+  if (box) {
+    let watch = new ResizeObserver(() => element && update(element));
+    watch.observe(Document.body);
 
-    let box = document.querySelector(selector);
-    if (box) {
-       let watch = new ResizeObserver(()=> element && update(element))
-      watch.observe(Document.body)
-      
-      let toolbar;
-      toolbar = box.querySelector(':scope .toolbar');
-      if(!toolbar ) toolbar = {offsetHeight: 0}
-      let element;
-      Document.addEventListener(event, e => {
-        update(e.target)
-        element = e.target;
-      });
-      
-      function update(element){
-        
-        box.style.display = "block";
-        box.style.top =
-          frameElement.offsetTop +
-          element.offsetTop +
-          Window.scrollY -
-          toolbar.offsetHeight +
-          "px";
-        box.style.left =
-          frameElement.offsetLeft +
-          element.offsetLeft +
-          Window.scrollX +
-          "px";
-        box.style.width = element.offsetWidth + "px";
-        box.style.height = element.offsetHeight + "px";
+    let toolbar;
+    toolbar = box.querySelector(":scope .toolbar");
+    let tagName = box.querySelector(":scope [tagName]");
+    if (!toolbar) toolbar = { offsetHeight: 0 };
+    let element;
+    Document.addEventListener(event, (e) => {
+      update(e.target);
+      element = e.target;
+    });
 
-        if ( element.offsetTop - toolbar.offsetHeight < 0 )
-          box.setAttribute("toolbar-overflow", "");
-        else box.removeAttribute("toolbar-overflow");
-      
+    Window.addEventListener("scroll", () => {
+      update(element);
+    });
+    function update(element) {
+      box.style.display = "block";
+      box.style.top =
+        frameElement.offsetTop +
+        element.offsetTop -
+        Window.scrollY -
+        toolbar.offsetHeight +
+        "px";
+      box.style.left =
+        frameElement.offsetLeft + element.offsetLeft + Window.scrollX + "px";
+      box.style.width = element.offsetWidth + "px";
+      box.style.height = element.offsetHeight + "px";
+
+      if (element.offsetTop - toolbar.offsetHeight < 0)
+        box.setAttribute("toolbar-overflow", "");
+      else box.removeAttribute("toolbar-overflow");
+
+      if (tagName&& tagName.innerHTML !== element.tagName) {
+        tagName.innerHTML = element.tagName;
+        for (let config of elementConfig) {
+          if (config.tagName && element.matches(config.selector)) {
+            if(tagName.innerHTML !== config.tagName)
+              tagName.innerHTML = config.tagName;
+            break;
+          }
+        }
       }
+        
     }
-
- 
-  } catch (error) {
-    console.error("toolbar didn't initlize", error);
   }
 }
 
-window.CoCreateToolbar = {init: toolbar}
+window.CoCreateToolbar = { init: toolbar };
 // window.addEventListener("load", toolbar);
