7ad2a2577936695ce9dedd479ac09d223fca9d3e
Frank Pagan <frank@cocreate.app>
2020-12-26 02:14:53 +0000

    update dist

---
+++ dist/CoCreate-boilerplate.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!*************************************!*\
+  !*** ./src/CoCreate-boilerplate.js ***!
+  \*************************************/
+eval("// component implementation:\n// 1. it should have programming api as well as html or attribute api\n// 2. Includes semantic//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9Db0NyZWF0ZS1ib2lsZXJwbGF0ZS8uL3NyYy9Db0NyZWF0ZS1ib2lsZXJwbGF0ZS5qcz80ZGE4Il0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQSIsInNvdXJjZXNDb250ZW50IjpbIi8vIGNvbXBvbmVudCBpbXBsZW1lbnRhdGlvbjpcbi8vIDEuIGl0IHNob3VsZCBoYXZlIHByb2dyYW1taW5nIGFwaSBhcyB3ZWxsIGFzIGh0bWwgb3IgYXR0cmlidXRlIGFwaVxuLy8gMi4gSW5jbHVkZXMgc2VtYW50aWMiXSwiZmlsZSI6Ii4vc3JjL0NvQ3JlYXRlLWJvaWxlcnBsYXRlLmpzLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/CoCreate-boilerplate.js\n");
+/******/ })()
+;
